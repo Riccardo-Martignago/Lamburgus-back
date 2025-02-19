5d511ad2d05f284f92984a8896cc170e37f94585
Riccardo-Martignago <martigriccardo97@gmail.com>
2025-02-19 18:33:18 +0100

    Added BookingRequest

---
+++ app/Http/Requests/BookingRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'pickup_location_id' => 'required|exists:locations,id',
+            'dropoff_location_id' => 'required|exists:locations,id',
+            'car_id' => 'required|exists:cars,id',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after:start_date',
+            'total_price' => 'required|numeric'
+        ];
+    }
+}
